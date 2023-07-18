f5afc0e0c5989113faaf163dd6db3b1a49ede969
Vadim Melnik <vmelnik@docsultant.com>
2023-07-18 17:34:54 +0300

    Added "message-log-view2" component based on Angular Material table (mat-table).

--- repromon_ng/src/app/app.module.ts
+++ repromon_ng/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { FeedbackScreenComponent } from './components/feedback-screen/feedback-screen.component';
 import { StudyHeaderComponent } from './components/study-header/study-header.component';
 import { MessageLogViewComponent } from './components/message-log-view/message-log-view.component';
+import { MessageLogView2Component } from './components/message-log-view2/message-log-view2.component';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
@@ -16,12 +21,18 @@ import { MessageLogViewComponent } from './components/message-log-view/message-l
     AppHeaderComponent,
     FeedbackScreenComponent,
     StudyHeaderComponent,
-    MessageLogViewComponent
+    MessageLogViewComponent,
+    MessageLogView2Component
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    NoopAnimationsModule
   ],
   providers: [DatePipe],
   bootstrap: [AppComponent]
---
+++ repromon_ng/src/app/components/message-log-view2/message-log-view2.component.ts
@@ -0,0 +1,129 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { AppConfig} from "../../config/AppConfig";
+import { FeedbackService } from '../../service/FeedbackService';
+import { MessageLogInfoDTO } from "../../model/MessageLogInfoDTO";
+import {PushMessageDTO} from "../../model/PushMessageDTO";
+import {PushListenerService} from "../../service/PushListenerService";
+import {SelectionModel} from "@angular/cdk/collections";
+
+
+@Component({
+  selector: 'message-log-view2',
+  templateUrl: './message-log-view2.component.html',
+  styleUrls: ['./message-log-view2.component.css']
+})
+export class MessageLogView2Component implements OnInit {
+  @Input() studyId: number = 0;
+  messageLog: MessageLogInfoDTO[] = [];
+  errorCount: number = 0;
+  warnCount: number = 0;
+
+  displayedColumns: string[] = ['index', 'date', 'time', 'level', 'provider', 'description'];
+  dataSource: MatTableDataSource<MessageLogInfoDTO> | null = null;
+  selection: any;
+
+  @ViewChild(MatSort) sort: MatSort | null = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+
+  constructor(
+    private datePipe: DatePipe,
+    private feedbackService: FeedbackService,
+    private pushListenerService: PushListenerService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.selection = new SelectionModel<any>(false, []);
+    this.pushListenerService.onMessage.subscribe(msg => {
+      if (msg.topic === 'feedback-log-refresh' && msg.body.study_id === this.studyId) {
+        this.reload();
+      }
+
+      if (msg.topic === 'feedback-log-add' && msg.body.study_id === this.studyId) {
+        this.addMessage(msg.body.message_id);
+      }
+    });
+    this.fetchMessageLog();
+  }
+
+  ngOnDestroy(): void {
+    this.pushListenerService.onMessage.unsubscribe();
+  }
+
+  async addMessage(message_id: number): Promise<void> {
+    console.log('addMessage(message_id=' + message_id + ')');
+    const msg = await this.feedbackService.getMessage(message_id).toPromise();
+    this.messageLog.push(msg as MessageLogInfoDTO);
+    this.updateCounters();
+  }
+
+  async clearMessages(mask: string): Promise<void> {
+    console.log("clearMessages(mask="+mask+")")
+    this.feedbackService.setMessageLogVisibility(this.studyId, false, mask).subscribe(
+      res => {
+        console.log("clearMessages res="+res);
+        //this.reload();
+      }
+    );
+    console.log("clearMessages(...) done")
+  }
+
+  async fetchMessageLog(): Promise<void> {
+    console.log('fetchMessageLog');
+    try {
+      this.feedbackService.getMessageLog(this.studyId).subscribe(
+        lst => {
+          this.messageLog = lst;
+          this.dataSource = new MatTableDataSource<MessageLogInfoDTO>(this.messageLog);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+          if( this.messageLog.length>0 )
+            this.selectItem(this.messageLog[this.messageLog.length-1])
+          this.updateCounters();
+        }
+      )
+    } catch (error) {
+      console.error('Failed to fetch message log:', error);
+    }
+  }
+
+  formatDate(timestamp: any): string {
+    return this.datePipe.transform(timestamp, 'yyyy-MM-dd') as string;
+  }
+
+  formatTime(timestamp: any): string {
+    return this.datePipe.transform(timestamp, 'HH:mm:ss') as string;
+  }
+
+  reload(): void {
+    console.log('reload');
+    this.fetchMessageLog();
+  }
+
+  async resetMessages(): Promise<void> {
+    console.log('resetMessages');
+    this.feedbackService.setMessageLogVisibility(this.studyId, true, '*').subscribe(
+      res => {
+        console.log("setMessageLogVisibility res="+res)
+        // this.reload();
+      }
+    );
+  }
+
+  selectItem(row: MessageLogInfoDTO): void {
+    console.log("selectItem: "+row.id)
+    this.selection.select(row);
+  }
+
+  private updateCounters(): void {
+    this.errorCount = this.messageLog.filter((message) => message.level === 'ERROR').length;
+    this.warnCount = this.messageLog.filter((message) => message.level === 'WARN').length;
+  }
+
+  protected readonly console = console;
+}
+
