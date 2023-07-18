c2a637815c7057b5a64b32d96ce83ee0fc115bea
Vadim Melnik <vmelnik@docsultant.com>
2023-07-18 21:45:10 +0300

    Replaced default HTML buttons with Angular Material ones

--- repromon_ng/src/app/app.module.ts
+++ repromon_ng/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
@@ -12,8 +14,7 @@ import { FeedbackScreenComponent } from './components/feedback-screen/feedback-s
 import { StudyHeaderComponent } from './components/study-header/study-header.component';
 import { MessageLogViewComponent } from './components/message-log-view/message-log-view.component';
 import { MessageLogView2Component } from './components/message-log-view2/message-log-view2.component';
-import {NoopAnimationsModule} from "@angular/platform-browser/animations";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
+    MatButtonModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatSortModule,
--- repromon_ng/src/app/components/message-log-view2/message-log-view2.component.ts
+++ repromon_ng/src/app/components/message-log-view2/message-log-view2.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
